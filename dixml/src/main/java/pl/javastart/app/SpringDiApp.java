0e175af88e2f29301f969e15c2309d9e8448d2d2
Pawel Urbanik <pawelurbanik94@gmail.com>
2019-03-30 20:20:29 +0100

    DI java config

--- dixml/src/main/java/pl/javastart/app/SpringDiApp.java
+++ dixml/src/main/java/pl/javastart/app/SpringDiApp.java
@@ -1,11 +1,12 @@
 package pl.javastart.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.javastart.beans.MessagePrinter;
 
 public class SpringDiApp {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         MessagePrinter messagePrinter = ctx.getBean(MessagePrinter.class);
         messagePrinter.printMessage();
         ctx.close();
