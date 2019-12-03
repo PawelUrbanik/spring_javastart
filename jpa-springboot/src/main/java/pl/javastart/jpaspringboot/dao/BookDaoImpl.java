7a168f467ac2ea945bac34cc2cda9a08398ce71c
Pawel Urbanik <pawelurbanik94@gmail.com>
2019-12-03 17:57:55 +0100

    Transaction Spring Boot JPA

--- jpa-springboot/src/main/java/pl/javastart/jpaspringboot/dao/BookDaoImpl.java
+++ jpa-springboot/src/main/java/pl/javastart/jpaspringboot/dao/BookDaoImpl.java
@@ -1,35 +1,28 @@
 package pl.javastart.jpaspringboot.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.javastart.jpaspringboot.model.Book;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.*;
 
 @Repository
 public class BookDaoImpl implements BookDao{
 
-    @PersistenceUnit
-    EntityManagerFactory factory;
+    @PersistenceContext
+    EntityManager entityManager;
 
     public BookDaoImpl() {
     }
 
+    @Transactional
     public void save(Book book) {
-        EntityManager manager = factory.createEntityManager();
-        EntityTransaction transaction = manager.getTransaction();
-        transaction.begin();
-        manager.persist(book);
-        transaction.commit();
-        manager.close();
+        entityManager.persist(book);
     }
 
     public Book get(int id) {
-        EntityManager manager = factory.createEntityManager();
-        Book book = manager.find(Book.class, id);
-        manager.close();
+        Book book = entityManager.find(Book.class, id);
+        entityManager.close();
         return book;
     }
 
