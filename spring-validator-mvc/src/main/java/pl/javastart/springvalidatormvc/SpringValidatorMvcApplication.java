b0cb8b005a2541bdb8f67113aa8c1f74f5e80cff
Pawel Urbanik <pawelurbanik94@gmail.com>
2020-01-26 18:40:45 +0100

    spring validator mvc

---
+++ spring-validator-mvc/src/main/java/pl/javastart/springvalidatormvc/SpringValidatorMvcApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.springvalidatormvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidatorMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidatorMvcApplication.class, args);
+    }
+
+}
