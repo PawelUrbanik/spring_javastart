2aa541d70908936afd7d8ca02751a57d4aa3e0f2
Pawel Urbanik <pawelurbanik94@gmail.com>
2020-01-28 19:38:17 +0100

    spring security boot

---
+++ spring-security-boot/src/main/java/pl/javastart/springsecurityboot/controller/HomeController.java
@@ -0,0 +1,17 @@
+package pl.javastart.springsecurityboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping
+    @ResponseBody
+    public String home(Model model)
+    {
+        return "Hello Spring Secutrity";
+    }
+}
